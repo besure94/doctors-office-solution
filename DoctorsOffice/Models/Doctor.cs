9d3319a136749b27f5a2c81b1b1683e2c6da0549
Brian Scherner <brianscherner@gmail.com>
2023-11-03 15:55:24 -0700

    Initial commit.

---
+++ DoctorsOffice/Models/Doctor.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+
+namespace DoctorsOffice.Models
+{
+  public class Doctor
+  {
+    public int DoctorId { get; set; }
+    public string Name { get; set; }
+    public string Speciality { get; set; }
+    public List<Patient> Patients { get; set; }
+  }
+}
---
+++ DoctorsOffice/Models/Patient.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using System;
+
+namespace DoctorsOffice.Models
+{
+  public class Patient
+  {
+    public int PatientId { get; set; }
+    public int DoctorId { get; set; }
+    public string Name { get; set; }
+    public DateTime Birthdate { get; set; }
+    public Doctor Doctor { get; set; }
+
+  }
+}
