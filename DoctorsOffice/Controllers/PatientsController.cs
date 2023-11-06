b99c3c740e01b7942bc7efd2feca5cd696314b16
Brian Scherner <brianscherner@gmail.com>
2023-11-06 10:22:15 -0800

    Add functionality to add a Patient to a Doctor.

---
+++ DoctorsOffice/Controllers/PatientsController.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Mvc;
+using DoctorsOffice.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace DoctorsOffice.Controllers
+{
+  public class PatientsController : Controller
+  {
+    private readonly DoctorsOfficeContext _db;
+    public PatientsController(DoctorsOfficeContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Patient> model = _db.Patients.Include(patient => patient.Doctor).ToList();
+      ViewBag.PageTitle = "View all patients";
+      return View(model);
+    }
+
+    public ActionResult Create()
+    {
+      ViewBag.DoctorId = new SelectList(_db.Doctors, "DoctorId", "Name");
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create (Patient patient)
+    {
+      if (patient.DoctorId == 0)
+      {
+        return RedirectToAction("Create");
+      }
+      _db.Patients.Add(patient);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+  }
+}
