2a6f177320b3dd0587b1d91719ec6bfef95e9b10
Brian Scherner <brianscherner@gmail.com>
2023-11-08 15:06:41 -0800

    Add functionality for many-to-many w/ Doctors and Specialties in Create and Details route + Details view.

--- DoctorsOffice/Controllers/DoctorsController.cs
+++ DoctorsOffice/Controllers/DoctorsController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
 
 namespace DoctorsOffice.Controllers
 {
@@ -28,10 +29,9 @@ namespace DoctorsOffice.Controllers
     }
 
     [HttpPost]
-    public ActionResult Create(Doctor doctor)
+    public ActionResult Create(Doctor doctor, int specialtyId)
     {
-
-      if (doctor.Name == null)
+      if (doctor.Name == null || specialtyId == 0)
       {
         return RedirectToAction("Create");
       }
@@ -39,7 +39,8 @@ namespace DoctorsOffice.Controllers
       {
         _db.Doctors.Add(doctor);
         _db.SaveChanges();
-        // _db.DoctorSpecialties.Add(new DoctorSpecialty() { SpecialtyId = doctor.SpecialtyId, DoctorId = doctor.DoctorId });
+        _db.DoctorSpecialties.Add(new DoctorSpecialty() { SpecialtyId = specialtyId, DoctorId = doctor.DoctorId });
+        _db.SaveChanges();
         return RedirectToAction("Index");
       }
     }
@@ -49,6 +50,10 @@ namespace DoctorsOffice.Controllers
       Doctor thisDoctor = _db.Doctors
       .Include(patient => patient.JoinEntities)
       .ThenInclude(doctor => doctor.Patient)
+      .FirstOrDefault(doctor => doctor.DoctorId == id);
+      Doctor thisDoctorsSpecialties = _db.Doctors
+      .Include(specialty => specialty.JoinClasses)
+      .ThenInclude(doctor => doctor.Specialty)
       .FirstOrDefault(doctor => doctor.DoctorId == id);
       return View(thisDoctor);
     }
